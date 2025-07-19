8de2b0bb8c4070972e0845f10a9f6b820d6817e3
saad09092012 <msaadatif@gmail.com>
2025-07-19 12:53:42 +0400

    Homework

---
+++ homework.py
@@ -0,0 +1,13 @@
+print(" Birthday of 5 friends\n")
+
+# Storing Birthday in variable and printing them
+a="13/02/92"
+print("aakash -",a)
+b="16/05/95"
+print("\nsunny-",b)
+c="06/05/92"
+print("\nsweety-",c)
+d="12/02/92"
+print("\npooja-",d)
+e="18/04/96"
+print("\nram-",e)
